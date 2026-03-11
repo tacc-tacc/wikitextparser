c2b07832d5609dbf9b0cce9127f7bf11b9a68cae
tacc-tacc <tomacastro@itba.edu.ar>
2026-03-11 03:31:19 -0300

    fix unquoted and half-quoted tag attrib parsing

--- wikitextparser/_spans.py
+++ wikitextparser/_spans.py
@@ -151,8 +151,8 @@ CONTROL_CHARS = rb'\x00-\x1f\x7f-\x9f'
 # https://www.w3.org/TR/html5/syntax.html#syntax-attributes
 ATTR_NAME = rb'(?<attr_name>[^' + SPACE_CHARS + CONTROL_CHARS + rb'"\'>/=]++)'
 EQ_WS = rb'[=\1][' + SPACE_CHARS + rb']*+'
-UNQUOTED_ATTR_VAL = rb'(?<attr_value>[^' + SPACE_CHARS + rb'"\'=<>`]++)'
-QUOTED_ATTR_VAL = rb'(?<quote>[\'"])(?<attr_value>.*?)(?P=quote)'
+UNQUOTED_ATTR_VAL = rb'[\'"]?(?<attr_value>[^' + SPACE_CHARS + rb'"\'=<>`/]++)'
+QUOTED_ATTR_VAL = rb'(?<quote>[\'"])(?<attr_value>[^"\'<>]*?)(?P=quote)'
 # May include character references, but for now, ignore the fact that they
 # cannot contain an ambiguous ampersand.
 ATTR_VAL = (
@@ -164,9 +164,9 @@ ATTR_VAL = (
     + rb']*+'
     + EQ_WS
     + rb'(?>'
-    + UNQUOTED_ATTR_VAL
-    + rb'|'
     + QUOTED_ATTR_VAL
+    + rb'|'
+    + UNQUOTED_ATTR_VAL
     + rb')'
     + rb'|(?<attr_value>)'  # empty attribute
     + rb')'
